968b7c16eb5ee1eb7c9e3091d827dacdd3838430
Viclor27322 <126909284+Viclor27322@users.noreply.github.com>
2024-06-20 17:44:25 -0600

    fist commit

---
+++ lambda/index.js
@@ -0,0 +1,270 @@
+/* *
+ * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+ * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+ * session persistence, api calls, and more.
+ * */
+const Alexa = require('ask-sdk-core');
+const i18n = require('i18next');
+const sprintf = require('i18next-sprintf-postprocessor');
+
+const languageStrings = {
+    en: {
+        translation: {
+            WELCOME_MESSAGE: 'Welcome, you can say "Tell me a Tesla fact" or "Help". Which would you like to try?',
+            HELLO_MESSAGE: 'Hello World victor!',
+            HELP_MESSAGE: 'You can ask me for a Tesla fact by saying "Tell me a Tesla fact". How can I help?',
+             CONTINUE_MESSAGE: '... If you want another fact, say "Tell me a Tesla fact"',
+            GOODBYE_MESSAGE: 'Goodbye!',
+            REFLECTOR_MESSAGE: 'You just triggered %s',
+            FALLBACK_MESSAGE: 'Sorry, I don\'t know about that. Please try again.',
+            ERROR_MESSAGE: 'Sorry, there was an error. Please try again.',
+            GET_FACT_MSG: 'Here\'s a Tesla fact: ',
+            FACTS: [
+                'The first Tesla Roadster used a Lotus Elise chassis.',
+                'Tesla\'s Model S can go from 0 to 60 mph in just 2.4 seconds.',
+                'Tesla was founded in 2003 by engineers Martin Eberhard and Marc Tarpenning.',
+                'The Model 3 is Tesla’s most affordable car.',
+                'Tesla’s Autopilot feature allows the car to steer, accelerate, and brake automatically.',
+                'Tesla has installed thousands of Supercharger stations worldwide.',
+                'The Tesla Model X has Falcon Wing doors.',
+                'Tesla’s Gigafactory in Nevada is one of the largest buildings in the world by footprint.'
+            ]
+        }
+    },
+    es: {
+        translation: {
+            WELCOME_MESSAGE: 'Bienvenido, puedes decir "Dime un dato sobre Tesla" o "Ayuda". ¿Qué te gustaría intentar?',
+            HELP_MESSAGE: 'Puedes pedirme un dato sobre Tesla diciendo "Dime un dato sobre Tesla". ¿Cómo te puedo ayudar?',
+            CONTINUE_MESSAGE:'... Si quieres otro dato di "Dime un dato sobre Tesla"',
+            HELLO_MESSAGE: 'Hola mundo victor!',
+            GOODBYE_MESSAGE: '¡Adiós!',
+            REFLECTOR_MESSAGE: 'Acabas de activar %s',
+            FALLBACK_MESSAGE: 'Lo siento, no sé sobre eso. Por favor, inténtalo de nuevo.',
+            ERROR_MESSAGE: 'Lo siento, hubo un error. Por favor, inténtalo de nuevo.',
+            GET_FACT_MSG: 'Aquí tienes un dato sobre Tesla: ',
+            FACTS: [
+                'El primer Tesla Roadster utilizó un chasis de Lotus Elise.',
+                'El Tesla Model S puede acelerar de 0 a 100 km/h en solo 2,4 segundos.',
+                'Tesla fue fundada en 2003 por los ingenieros Martin Eberhard y Marc Tarpenning.',
+                'El Model 3 es el coche más asequible de Tesla.',
+                'La función Autopilot de Tesla permite que el coche dirija, acelere y frene automáticamente.',
+                'Tesla ha instalado miles de estaciones Supercharger en todo el mundo.',
+                'El Tesla Model X tiene puertas Falcon Wing.',
+                'La Gigafactory de Tesla en Nevada es uno de los edificios más grandes del mundo por superficie.'
+            ]
+        }
+    }
+};
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speakOutput = requestAttributes.t('WELCOME_MESSAGE');
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+
+
+
+const HelloWorldIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speakOutput = requestAttributes.t('HELLO_MESSAGE');
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+
+const TeslaFactIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'TeslaFactIntent';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const facts = requestAttributes.t('FACTS');
+        const factIndex = Math.floor(Math.random() * facts.length);
+        const randomFact = facts[factIndex];
+        const speakOutput = requestAttributes.t('GET_FACT_MSG') + randomFact + '... Si quieres otro dato di "Dime un dato sobre Tesla"';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speakOutput = requestAttributes.t('HELP_MESSAGE');
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speakOutput = requestAttributes.t('GOODBYE_MESSAGE');
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
+ * It must also be defined in the language model (if the locale supports it)
+ * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * */
+
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speakOutput = requestAttributes.t('FALLBACK_MESSAGE');
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speakOutput = requestAttributes.t('ERROR_MESSAGE');
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+// This request interceptor will log all incoming requests to this lambda
+const LoggingRequestInterceptor = {
+    process(handlerInput) {
+        console.log(`Incoming request: ${JSON.stringify(handlerInput.requestEnvelope.request)}`);
+    }
+};
+
+// This response interceptor will log all outgoing responses of this lambda
+const LoggingResponseInterceptor = {
+    process(handlerInput, response) {
+      console.log(`Outgoing response: ${JSON.stringify(response)}`);
+    }
+};
+
+// This request interceptor will bind a translation function 't' to the requestAttributes.
+const LocalizationInterceptor = {
+  process(handlerInput) {
+    const localizationClient = i18n.use(sprintf).init({
+      lng: handlerInput.requestEnvelope.request.locale,
+      fallbackLng: 'en',
+      overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
+      resources: languageStrings,
+      returnObjects: true
+    });
+
+    const attributes = handlerInput.attributesManager.getRequestAttributes();
+    attributes.t = function (...args) {
+      return localizationClient.t(...args);
+    }
+  }
+}
+
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        TeslaFactIntentHandler,
+        HelloWorldIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler
+    )
+    .addErrorHandlers(ErrorHandler)
+    .addRequestInterceptors(LoggingRequestInterceptor, LocalizationInterceptor)
+    .addResponseInterceptors(LoggingResponseInterceptor)
+    .withCustomUserAgent('sample/hello-world/v1.2')
+    .lambda();
